0f891e6d990331cfc0d7b895ede3dc71cc08fbe6
Davi Souza <davisouzavieiradacosta@gmail.com>
2024-01-31 10:59:23 -0300

    Adicionando o código da calculadora já funcionando

--- src/script.js
+++ src/script.js
@@ -2,6 +2,49 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //ESCREVA SEU CÓDIGO AQUI!!
+function verificarErro(resultado) {
+  if (resultado === undefined || resultado > 1000000) {
+    return 'ERRO'
+  } else {
+    return resultado
+  }
+
+}
+
+
+function Calculadora(num1, num2, operador) {
+  let resultado
+  num1 = Number(num1)
+  num2 = Number(num2)
+
+  switch (operador) {
+    case '+':
+      resultado = num1 + num2
+      return verificarErro(resultado);
+    case '-':
+      resultado = num1 - num2
+      return verificarErro(resultado);
+    case '/':
+      resultado = num1 / num2
+      return verificarErro(resultado);
+    case '*':
+      resultado = num1 * num2
+      return verificarErro(resultado);
+    case 'e':
+      resultado = num1
+
+      if (num2 === 0) {
+        resultado = 0
+      } else {
+        for (let contador = 1; contador != num2; contador ++) {
+          resultado *= num1
+        }
+      }
+      
+      return verificarErro(resultado);
+  }
+
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
